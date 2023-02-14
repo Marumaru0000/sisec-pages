40d24325666458a00c807e775b8b5c99a764c20a
Marumaru0000 <109151313+Marumaru0000@users.noreply.github.com>
2023-02-14 14:02:36 +0900

    Add files via upload

---
+++ js/Slideshow.js
@@ -0,0 +1,16 @@
+setTimeout(function () {
+    $('.slideshow').vegas({
+        slides: [
+            { src: 'images/SummerCamp.jpg' },
+            { src: 'images/grasses.jpg' },
+            { src: 'images/WelcomeParty.jpg' },
+            { src: 'images/night_view.jpg'},
+        ],
+		transition: 'blur',		//zoomOutもかっこいい
+		animation: 'kenburns',	//中央から拡大される
+		delay: 5000,			//1枚ごとの時間
+    });
+},5500); //タイミングを調整
+setTimeout(function(){
+    $('.LogoAnimation').fadeOut();
+},5000); //タイミングを調整
